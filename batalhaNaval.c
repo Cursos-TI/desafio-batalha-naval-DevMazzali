6b6f1317ccb33cc833ceba88b9f042880ffea59a
Mazzali <gamedesignerrafael@gmail.com>
2025-04-05 23:45:23 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void tabuleiro (int tabuleiro[10][10]) {
-    char line [10] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+#define TAMANHO_TABULEIRO 10
+#define AGUA 0
+#define NAVIO 3
+
+void tabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    char line[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     printf(" TABULEIRO BATALHA NAVAL \n");
     printf("  ");
-    for(int j = 0; j < 10; j++){
+    for (int j = 0; j < 10; j++) {
         printf("%c ", line[j]); // Imprime os números das colunas
     }
-    printf("\n");
+    printf("\n"); // Pula uma linha para começar a imprimir o tabuleiro
     
     for (int i = 0; i < 10; i++) {
-        printf("%2d ", i+1); // Aqui o comando i+1 serve para imprimir os numeros das linhas começando em 1, ao invez de começar em 0
+        printf("%2d ", i + 1); // Imprime os números das linhas começando em 1
         for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
@@ -18,19 +23,85 @@ void tabuleiro (int tabuleiro[10][10]) {
     }
 }
 
-
-int main () {
-    int ship[10][10] = {0}; // Inicializa o tabuleiro com 0 (água)
+int main() {
+    int naval_battle[10][10] = {AGUA}; // Inicializa o tabuleiro com 0 (água)
     
-    ship [1][1] = 3;
-    ship [1][2] = 3;
-    ship [1][3] = 3;
+    // Inicializando os navios base com 3 (navio)
+    for (int i = 4; i < 5; i++) {
+        for (int j = 6; j < 9; j++) {
+            if (i >= 0 && i < TAMANHO_TABULEIRO && j >= 0 && j < TAMANHO_TABULEIRO) {
+                naval_battle[i][j] = NAVIO;
+        } 
+      }  
+      
+      for (int i = 8; i < 9; i++) {
+        for (int j = 2; j < 5; j++) {
+            if (i >= 0 && i < TAMANHO_TABULEIRO && j >= 0 && j < TAMANHO_TABULEIRO) {
+              naval_battle[i][j] = NAVIO;
+        } 
+      }  
 
-    ship [6][4] = 3;
-    ship [7][4] = 3;
-    ship [8][4] = 3;
-    
-    tabuleiro(ship); // Chama a função para imprimir o tabuleiro
+    for (int i = 0; i < 3; i++) {
+        naval_battle[i][i] = NAVIO;
+    }
+    for (int i = 6; i < 9; i++) {
+        naval_battle[i][i] = NAVIO;
+    }
+        
+    // Áreas de habilidade
+    int cone_column = 3;
+    for (int i = cone_column - cone_column; i < cone_column; i++) {
+        for (int j = cone_column - i; j <= cone_column + i; j++) {
+            if (i >= 0 && i < TAMANHO_TABULEIRO && j >= 0 && j < TAMANHO_TABULEIRO) {
+                if (naval_battle[i][j] == NAVIO) {
+                    naval_battle[i][j] = 5; // Marca o navio atingido
+                } else {
+                    naval_battle[i][j] = 1; // Marca a área atingida
+                }
+            }
+        }
+    }
+
+    int cruz_coluna = 5;
+    for (int i = cruz_coluna; i < cruz_coluna + 4; i++) {
+        if (i >= 0 && i < TAMANHO_TABULEIRO && cruz_coluna >= 0 && cruz_coluna < TAMANHO_TABULEIRO) {
+            naval_battle[i][cruz_coluna] = 1;
+        }
+    }
+    for (int x = cruz_coluna + 1; x == cruz_coluna + 1; x++) {
+        for (int j = cruz_coluna - x + 4; j <= cruz_coluna + x - 4; j++) {
+            if (j >= 0 && j < TAMANHO_TABULEIRO && x >= 0 && x < TAMANHO_TABULEIRO) {
+                if (naval_battle[x][j] == NAVIO) {
+                    naval_battle[x][j] = 5; // Marca o navio atingido
+                } else {
+                    naval_battle[x][j] = 1; // Marca a área atingida
+                }
+            }
+        }
+    }
+
+    int octaedro_coluna = 8;
+    for (int i = octaedro_coluna - 6; i < octaedro_coluna - 3; i++) {
+        if (i >= 0 && i < TAMANHO_TABULEIRO && octaedro_coluna >= 0 && octaedro_coluna < TAMANHO_TABULEIRO) {
+            naval_battle[i][octaedro_coluna] = 1;
+        }
+    }
+    for (int x = octaedro_coluna - 5; x == octaedro_coluna - 5; x++) {
+        for (int y = octaedro_coluna - 1; y <= octaedro_coluna + 1; y++) {
+            if (y >= 0 && y < TAMANHO_TABULEIRO && x >= 0 && x < TAMANHO_TABULEIRO) {
+                if (naval_battle[x][y] == NAVIO) {
+                naval_battle[x][y] = 5;
+            } 
+            else {
+                naval_battle[x][y] = 1;
+            }
+        }
+    }
+}
+
+
+    // Chama a função para imprimir o tabuleiro
+    tabuleiro(naval_battle);
     
     return 0;
 }
